d18a53c266caaec77a95f6c201e5c0ff3a5fd0ec
kincy25-hub <jesiflora611@gmail.com>
2026-08-23 13:14:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "IT Help Desk Backend is running successfully"
+  });
+});
+
+app.get("/api/test", (req, res) => {
+  res.json({
+    message: "Backend API is working"
+  });
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
