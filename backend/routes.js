7fab68c9a9fed254ed416f604f4e5e9ef885f00c
kincy25-hub <jesiflora611@gmail.com>
2026-08-23 13:18:04 +0530

    Update routes.js

--- backend/routes.js
+++ backend/routes.js
@@ -1,1 +1,17 @@
+const express = require("express");
 
+const router = express.Router();
+
+router.post("/register", (req, res) => {
+  res.json({
+    message: "Register API working"
+  });
+});
+
+router.post("/login", (req, res) => {
+  res.json({
+    message: "Login API working"
+  });
+});
+
+module.exports = router;
