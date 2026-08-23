b3a89f35dd87f9587287576ea33f104115d56b00
kincy25-hub <jesiflora611@gmail.com>
2026-08-23 13:17:22 +0530

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,1 +1,19 @@
+const mysql = require("mysql2");
 
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "it_helpdesk"
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log("Database connection failed:", err.message);
+    return;
+  }
+
+  console.log("MySQL connected successfully");
+});
+
+module.exports = db;
