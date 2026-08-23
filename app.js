54e4e3d93e98328d85b3bb8768e23c5b88c4cd27
jenifa1702-rjj <jenifa1702@gmail.com>
2026-08-23 12:21:51 +0530

    Set up routing with React Router in App component

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import CreateTicket from "./pages/CreateTicket";
+import MyTickets from "./pages/MyTickets";
+import TicketDetails from "./pages/TicketDetails";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/create-ticket" element={<CreateTicket />} />
+        <Route path="/tickets" element={<MyTickets />} />
+        <Route path="/tickets/:id" element={<TicketDetails />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
