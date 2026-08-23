f682ff223510370746670e45adb976335526b907
jenifa1702-rjj <jenifa1702@gmail.com>
2026-08-23 13:17:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
+const express = require("express");
+const cors = require("cors");
 
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "IT Help Desk Backend is running successfully"
+  });
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
